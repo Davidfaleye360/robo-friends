0317f9ba989ec46aebb1802694a73205b2dc0ce2
David <dfaleye360@gmail.com>
2023-12-25 19:34:22 -0500

    complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import CardList from './Card-list';
+import SearchBox from './Search-Box';
+import Scroll from "./Scroll"
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            robots: [],
+            searchfield: ''
+        }
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+            .then(users => {
+                this.setState({ robots: users })
+            })
+    }
+
+    onSearchChange = (e) => {
+        this.setState({ searchfield: e.target.value })
+    }
+
+    render() {
+        const { robots, searchfield } = this.state;
+        const filteredRobots = robots.filter(robot => {
+            return robot.name.toLowerCase().includes(searchfield.toLowerCase())
+        })
+        return !robots.length ?
+            <h1>Loading. . .</h1> :
+            (
+                <div className='tc'>
+                    <h1 className='f2'>Robo Friends</h1>
+                    <SearchBox searchChange={this.onSearchChange} />https://developer.mozilla.org/en-US/docs/Web/API/History_API#Adding_and_modifying_history_entries
+                    <Scroll>
+                        <CardList robots={filteredRobots} />
+                    </Scroll>
+                </div>
+            );
+    }
 }
 
 export default App;
